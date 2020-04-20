00f22734bfda3529d0f4d3c0fdca55af48101f95
alansai <alan.typing@gmail.com>
2020-04-19 21:26:31 -0400

    Fixed it AgAiN !!!

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy, os
+from flask_sqlalchemy import SQLAlchemy
+import os
   
 def getApp():
     return app
